0e517f4bf16218d49c11a0d2827c86408f8c3841
mkarimidevops <mkarimidevops@gmail.com>
2022-02-27 23:03:31 -0600

    test verified

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestCards(t *testing.T) {
 	d := newDeck()
 
-	if len(d) != 16 {
-		t.Errorf("Expected response include 16 cards %v", len(d))
+	if len(d) != 52 {
+		t.Errorf("Expected response include 52 cards %v", len(d))
 	}
 }
